3f619e7f9eb666b0a0fc1c015d51aa1733b37524
Trevor Spreadbury <trevorspreadbury@gmail.com>
2025-03-26 14:16:31 -0500

    Added arguments for running select models and running locally

--- scripts/llm-exploration.py
+++ scripts/llm-exploration.py
@@ -1,27 +1,32 @@
 import pandas as pd
 import torch
+import time
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from pathlib import Path
+from huggingface_hub import login
+import os
 
-BASE_DIR = Path(__file__).resolve().parent
+login(token=os.environ["HF_TOKEN"])
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 MAX_NEW_TOKENS = 16000
 
 models = [
-    # "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
+    "deepseek-ai/DeepSeek-R1-Distill-Qwen-32B",
     "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B",
-    "mistralai/Ministral-8B-Instruct-2410",
+    # "mistralai/Ministral-8B-Instruct-2410",
     # "mistralai/Mistral-Small-3.1-24B-Instruct-2503",
     # "google/gemma-3-27b-it",
-    "google/gemma-3-4b-it",
-    "google/gemma-3-1b-it",
-    # "Qwen/QwQ-32B",
-    "Qwen/Qwen2.5-Coder-7B-Instruct",
+    # "google/gemma-3-4b-it",
+    # "google/gemma-3-1b-it",
+    "Qwen/QwQ-32B",
+    #  "Qwen/Qwen2.5-Coder-7B-Instruct",
     # "Qwen/Qwen2.5-Coder-14B-Instruct",
-    "Qwen/Qwen2.5-Coder-0.5B-Instruct",
+    # "Qwen/Qwen2.5-Coder-0.5B-Instruct",
     "Qwen/Qwen2.5-7B-Instruct",
     "meta-llama/Llama-3.1-8B-Instruct",
 ]
-with (BASE_DIR / "data" / "prompts" / "instructions.md").open(
+with (BASE_DIR / "data" / "prompts" / "combined-prompt.md").open(
     "r", encoding="utf-8"
 ) as f:
     instruction_text = f.read()
@@ -39,6 +44,7 @@ def run_auto_model_for_causalLM_on_prompt(
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     results = []
     counter = 0
+    output_file = f"{model_name.split('/')[-1]}-results.csv"
 
     # Process each row in the CSV
     for _, row in data.iterrows():
@@ -46,8 +52,7 @@ def run_auto_model_for_causalLM_on_prompt(
         input_text = row["text"]
 
         messages = [
-            {"role": "system", "content": instructions},
-            {"role": "user", "content": input_text},
+            {"role": "user", "content": f"{instructions}\n{input_text}"},
         ]
 
         formatted_text = tokenizer.apply_chat_template(
@@ -55,8 +60,10 @@ def run_auto_model_for_causalLM_on_prompt(
             tokenize=False,
             add_generation_prompt=True,
         )
+        start_time = time.perf_counter()
         model_inputs = tokenizer([formatted_text], return_tensors="pt").to(model.device)
         generated_ids = model.generate(**model_inputs, max_new_tokens=MAX_NEW_TOKENS)
+        end_time = time.perf_counter()
         generated_ids = [
             output_ids[len(input_ids) :]
             for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
@@ -70,6 +77,7 @@ def run_auto_model_for_causalLM_on_prompt(
                 "input_text": input_text,
                 "model_name": model_name,
                 "response": response,
+                "inference_time": end_time - start_time
             }
         )
 
@@ -78,10 +86,10 @@ def run_auto_model_for_causalLM_on_prompt(
         if counter % flush_every == 0:
             # Convert current results list into a DataFrame and append to CSV.
             pd.DataFrame(results).to_csv(
-                "results.csv",
+                output_file,
                 mode="a",
                 index=False,
-                header=not pd.io.common.file_exists("results.csv"),
+                header=False,
             )
             print(f"Flushed {counter} samples to results.csv")
             results = []  # clear the list after flushing
@@ -89,16 +97,17 @@ def run_auto_model_for_causalLM_on_prompt(
     # Write any remaining results after processing all rows
     if results:
         pd.DataFrame(results).to_csv(
-            "results.csv",
+            output_file,
             mode="a",
             index=False,
-            header=not pd.io.common.file_exists("results.csv"),
+            header=False,
         )
         print(f"Finished processing all samples for {model_name}")
 
 
 if __name__ == "__main__":
     import submitit
+    import argparse
 
     executor = submitit.AutoExecutor(folder=BASE_DIR / "logs")
     executor.update_parameters(
@@ -109,9 +118,22 @@ if __name__ == "__main__":
         slurm_job_name="commercial-debt-tracker",
         slurm_array_parallelism=6,
     )
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--local", action="store_true")
+    ap.add_argument("--models", nargs="+", default=models)
+    args = ap.parse_args()
 
     with executor.batch():
-        for model in models:
-            executor.submit(
-                run_auto_model_for_causalLM_on_prompt, model, instruction_text, input_df
-            )
+        for model in args.models:
+            if args.local:
+                run_auto_model_for_causalLM_on_prompt(model, instruction_text, input_df)
+            else:
+                executor.submit(
+                    run_auto_model_for_causalLM_on_prompt,
+                    model,
+                    instruction_text,
+                    input_df,
+                )
+
+
+
